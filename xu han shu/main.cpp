33799e081ca5a34f929b573728cfdf5ca3e37b02
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-06-27 11:21:04 +0800

    Update main.cpp
    xu han shu

--- xu han shu/main.cpp
+++ xu han shu/main.cpp
@@ -1,15 +1,40 @@
-//
-//  main.cpp
-//  xu han shu
-//
-//  Created by s20181102929 on 2019/6/27.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
 #include <iostream>
+using namespace std;
+class CMyClassA {
+    int val;
+public:
+    CMyClassA(int);
+    void virtual print();
+};
+CMyClassA::CMyClassA(int arg) {
+    val = arg;
+    printf("A:%d\n", val);
+}
+void CMyClassA::print() {
+    printf("%d\n", val);
+    return;
+}
+class CMyClassB:public CMyClassA{
+    private :
+    int a;
+    public :
+    void print(){
+        printf("%d\n", a);
+    }
+    CMyClassB(int a,int b=3):CMyClassA(a*b),a(a){
+        printf("B:%d\n", a);
+    }
+};
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main(int argc, char** argv) {
+    CMyClassA a(3), *ptr;
+    CMyClassB b(5);
+    ptr = &a;
+    ptr->print();
+    a = b;
+    a.print();
+    ptr = &b;
+    ptr->print();
     return 0;
 }
